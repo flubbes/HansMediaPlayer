ddf617669914bf9596a018d11a2a04c20f541da0
Fabian Berg <info@fabian-berg.de>
2014-05-26 13:18:47 +0200

    moved the hanssong file

---
+++ Hans/Library/HansSong.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hans
+{
+    public class HansSong
+    {
+
+        public HansSong(string path)
+        {
+            Path = path;
+        }
+
+        public long Length
+        {
+            get { return 0; }
+        }
+
+        public long Position
+        {
+            get { return 0; }
+        }
+
+        public String Path { get; set; }
+    }
+}
