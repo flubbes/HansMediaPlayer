b6843042aed42499fe9b7c1eba75cd4bd9c9d565
Fabian Berg <info@fabian-berg.de>
2014-06-02 23:18:34 +0200

    jos: never commit something with an error in it! ;D No means meow

--- Hans/Services/YouTube/YouTubeTrack.cs
+++ Hans/Services/YouTube/YouTubeTrack.cs
@@ -9,6 +9,13 @@ namespace Hans.Services.YouTube
 {
     class YouTubeTrack : IOnlineServiceTrack
     {
-           
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Mp3Url { get; set; }
+        public string DisplayName { get; private set; }
+        public string GetFileName()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
