8a9d39ae5c80b490b30ec3d86b5200c432c714ff
Fabian Berg <info@fabian-berg.de>
2014-06-03 21:57:59 +0200

    added some comments

--- Hans/Modules/DatabaseModule.cs
+++ Hans/Modules/DatabaseModule.cs
@@ -9,8 +9,14 @@ using Ninject.Modules;
 
 namespace Hans.Modules
 {
+    /// <summary>
+    /// The database module that loads all components of the hans player
+    /// </summary>
     public class DatabaseModule : NinjectModule
     {
+        /// <summary>
+        /// Loads all modules for the database
+        /// </summary>
         public override void Load()
         {
             Bind<IDatabaseSaver>().To<FlatFileDatabaseSaver>();
