ee037d98812832ceff59602ec9db0912a51c4faf
Fabian Berg <info@fabian-berg.de>
2014-06-06 01:18:50 +0200

    added

---
+++ Hans/Modules/AudioModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hans.Library;
+using Ninject.Modules;
+
+namespace Hans.Modules
+{
+    public class AudioModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IAudioLoader>().To<DefaultAudioLoader>();
+        }
+    }
+}
