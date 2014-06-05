804b47bc8f5498fdbe27c7fdd4a839744ea01354
Fabian Berg <info@fabian-berg.de>
2014-06-05 11:24:40 +0200

    added file

---
+++ Hans/Database/FlatFile/FlatFilePlaylistStore.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Hans.Library;
+
+namespace Hans.Database
+{
+    public class FlatFilePlaylistStore : IPlaylistStore
+    {
+        public void Add(HansPlaylist item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(HansPlaylist item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<HansPlaylist> GetEnumerable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(HansPlaylist item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
