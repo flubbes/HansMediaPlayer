ffccf73c826f11f81f7edf5ec96e70968683843f
Fabian Berg <info@fabian-berg.de>
2014-06-06 01:16:56 +0200

    implemented some methods

--- Hans/Web/DownloadRequest.cs
+++ Hans/Web/DownloadRequest.cs
@@ -7,10 +7,26 @@ namespace Hans.Web
     public class DownloadRequest
     {
         public string Uri { get; set; }
-        public string DestinationPath { get; set; }
-
+        public string DestinationDirectory { get; set; }
+        public string FileName { get; set; }
         public IDownloader Downloader { get; set; }
-
         public IOnlineServiceTrack OnlineServiceTrack { get; set; }
+
+        public string GetAbsolutePath()
+        {
+            return Path.GetFullPath(GetRelativePath());
+        }
+
+        public string GetRelativePath()
+        {
+            var directory = DestinationDirectory;
+            var fileName = GetSafeFileName();
+            return Path.Combine(directory, fileName);
+        }
+
+        private string GetSafeFileName()
+        {
+            return FileName.RemoveIllegalCharacters();
+        }
     }
 }
