9f9a82fb9aa9b10108748ee0ac03cb455e3f7985
Fabian Berg <info@fabian-berg.de>
2014-06-05 11:24:52 +0200

    added file

---
+++ Hans/Database/IStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Hans.Database
+{
+    public interface IStore<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+        IEnumerable<T> GetEnumerable();
+        void Update(T item);
+    }
+}
