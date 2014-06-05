22d5ca4b83a4b257fa32e9247b48b08189743c93
Jos Verra <Tunderbird1@live.nl>
2014-06-05 18:29:13 +0200

    Added removeIllegalCharacters method

---
+++ Hans/General/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace Hans.General
+{
+    public static class StringExtensions
+    {
+        public static string RemoveIllegalCharacters(this string s)
+        {
+            var newName = string.Empty;
+            var invalidCharacters = new List<char>(Path.GetInvalidFileNameChars());
+            invalidCharacters.AddRange(Path.GetInvalidPathChars());
+            return s.Where(c => !invalidCharacters.Contains(c)).Aggregate(newName, (current, c) => current + c);
+        }
+    }
+}
