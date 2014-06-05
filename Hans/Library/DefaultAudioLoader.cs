530650edc78cfc8cb19e96a9b266e84fa5115545
Fabian Berg <info@fabian-berg.de>
2014-06-06 01:18:45 +0200

    added

---
+++ Hans/Library/DefaultAudioLoader.cs
@@ -0,0 +1,18 @@
+﻿using Hans.Database.Songs;
+using NAudio.Wave;
+
+namespace Hans.Library
+{
+    class DefaultAudioLoader : IAudioLoader
+    {
+        public AudioFileReader Load(HansSong song)
+        {
+            return new AudioFileReader(song.FilePath);
+        }
+
+        public void UnLoad()
+        {
+            
+        }
+    }
+}
