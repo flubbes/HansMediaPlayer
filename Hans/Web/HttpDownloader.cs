0f9a6912f1fb8807716c6386766663c753e94f66
Fabian Berg <info@fabian-berg.de>
2014-06-14 21:43:34 +0200

    rewritten

--- Hans/Web/HttpDownloader.cs
+++ Hans/Web/HttpDownloader.cs
@@ -1,18 +1,40 @@
-﻿using System;
+﻿using Hans.FileSystem;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
+using System.Threading;
 
 namespace Hans.Web
 {
     public class HttpDownloader : IDownloader
     {
+        private bool _abort;
+        private Thread _thread;
+
+        public event EventHandler Failed;
+
         public int Progress { get; private set; }
 
         public void Abort()
         {
-            
+            _thread.Abort();
         }
 
-        public event EventHandler Failed;
+        public void Start(DownloadRequest request)
+        {
+            //TODO clean code
+            var fileSystem = request.FileSystem;
+            var filePath = request.FileSystem.Get.CombinationFullPath(request.DestinationDirectory, request.FileName);
+            if (request.Uri == null
+                || fileSystem.Exists.File(filePath) && !fileSystem.Can.ReadWrite.File(filePath))
+            {
+                OnFailed();
+                return;
+            }
+            _thread = new Thread(() => Download(request, filePath)) { IsBackground = true };
+            _thread.Start();
+        }
 
         protected virtual void OnFailed()
         {
@@ -23,22 +45,34 @@ namespace Hans.Web
             }
         }
 
-        public void Start(DownloadRequest request)
+        private bool CanAccessFile(string filePath)
         {
-            if (request.Uri != null)
+            throw new NotImplementedException();
+        }
+
+        private void Download(DownloadRequest request, string absoluteFilePath)
+        {
+            //TODO clean code
+            var fileSystem = request.FileSystem;
+            using (var fileWriter = fileSystem.Open.ForWriting(absoluteFilePath))
             {
-                OnFailed();
-                return;
+                using (var webResponse = WebRequest.CreateHttp(request.Uri).GetResponse())
+                {
+                    var g = (int)webResponse.ContentLength;
+                    using (var responseStream = webResponse.GetResponseStream())
+                    {
+                        var buffer = new byte[4096];
+                        var written = 0;
+                        do
+                        {
+                            written = responseStream.Read(buffer, 0, buffer.Length);
+                            fileWriter.Write(buffer, 0, written);
+                            Progress = written * 100 / g;
+                        } while (written > 0 && !_abort);
+                    }
+                }
             }
-            var httpRequest = HttpWebRequest.CreateHttp(request.Uri);
-            var webResponse = httpRequest.GetResponse();
-            var responseStream = webResponse.GetResponseStream();
-            var buffer = new byte[4096];
-            var position = 0;
-            while (responseStream.Read(buffer, position, buffer.Length) != 0)
-            {
-                
-            }
+            Progress = 100;
         }
     }
 }
