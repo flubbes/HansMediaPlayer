69979e479296ef757399ca72db17b2228d1dc11f
Fabian Berg <info@fabian-berg.de>
2014-06-15 17:09:54 +0200

    comments for the hansplaylist and the playlist store

--- Hans/Database/Playlists/HansPlaylist.cs
+++ Hans/Database/Playlists/HansPlaylist.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Hans.Database.Songs;
+using System;
 using System.Collections.Generic;
-using Hans.Database.Songs;
 
 namespace Hans.Database.Playlists
 {
+    /// <summary>
+    /// A playlist from the hans audio player
+    /// </summary>
     public class HansPlaylist
     {
+        /// <summary>
+        /// The id of the playlist
+        /// </summary>
         public Guid Id;
 
+        /// <summary>
+        /// The name of the playlist
+        /// </summary>
         public string Name { get; set; }
     }
 }
