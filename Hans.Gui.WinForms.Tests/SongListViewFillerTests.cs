d254c2fa7e7ad5b83b0f5b256d09b9db0cb6f082
Fabian Berg <info@fabian-berg.de>
2014-08-11 00:25:08 +0200

    added songListViewFiller

--- Hans.Gui.WinForms.Tests/SongListViewFillerTests.cs
+++ Hans.Gui.WinForms.Tests/SongListViewFillerTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Windows.Forms;
+using FluentAssertions;
+using Hans.Core.Database.Songs;
+using NUnit.Framework;
 
 namespace Hans.Gui.WinForms.Tests
 {
@@ -6,15 +9,39 @@ namespace Hans.Gui.WinForms.Tests
     public class SongListViewFillerTests
     {
         [Test]
-        public void CanCreateSongListViewFiller()
+        public void CanFillListView()
         {
-            new SongListViewFiller();
+            var filler = new SongListViewFiller();
+            var listView = new ListView();
+            var item = new HansSong("test");
+            filler.Fill(new SongListViewFillRequest
+            {
+                ListView = listView,
+                Songs = new[] { item}
+            });
+            listView.Items.Count.Should().Be(1);
+            var firstItem = listView.Items[0];
+            firstItem.SubItems[0].Text.Should().Be(item.Artist);
+            firstItem.SubItems[1].Text.Should().Be(item.Title);
         }
+    }
 
-         
+    public struct SongListViewFillRequest
+    {
+        public ListView ListView { get; set; }
+        public HansSong[] Songs { get; set; }
     }
 
     public class SongListViewFiller
     {
+        public void Fill(SongListViewFillRequest songs)
+        {
+            foreach (var song in songs.Songs)
+            {
+                var item = new ListViewItem(song.Artist);
+                item.SubItems.Add(song.Title);
+                songs.ListView.Items.Add(item);
+            }
+        }
     }
 }
